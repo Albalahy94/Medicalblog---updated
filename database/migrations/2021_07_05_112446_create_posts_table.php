5e3f2f8a964adb7c44de722c0aaa78983e6464c8
albalahy94 <sh.elbalahy@gmail.com>
2021-07-17 04:09:03 +0200

    file added

--- database/migrations/2021_07_05_112446_create_posts_table.php
+++ database/migrations/2021_07_05_112446_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->integer('user_id');
             $table->string('name');
             $table->string('description');
+            $table->string('file');
             $table->string('tag');
             $table->string('category');
             $table->timestamps();
