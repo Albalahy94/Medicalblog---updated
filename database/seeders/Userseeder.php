fa2c2ccd47a4ffa597fce033fbd84bcda1fc1803
albalahy94 <sh.elbalahy@gmail.com>
2021-07-27 06:46:58 +0200

    complete

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -16,13 +16,7 @@ class Userseeder extends Seeder
     {
         //
         DB::table('users')->insert(
-            [
-                'name' => 'said',
-                'admin_status' => '0',
-                'pending' => '1',
-                'email' => 'said@said',
-                'password'  => bcrypt('01230123'),
-            ],
+          
             [
                 'name' => 'said',
                 'admin_status' => '0',
