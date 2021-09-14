996c4ad41305c0bb6095f105d2334bed86a150af
albalahy94 <sh.elbalahy@gmail.com>
2021-09-14 13:52:41 +0200

    new  commit

--- app/Events/NotifyUsers.php
+++ app/Events/NotifyUsers.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Carbon\Carbon;
 
-class NotifyUsers
+class NotifyUsers implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
